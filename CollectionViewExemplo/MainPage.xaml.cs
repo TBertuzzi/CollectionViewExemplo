1edd2261478a5d6d8a964a10f9fcaa125d9c6e3a
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-01-30 10:49:05 -0200

    :tada: Subindo exemplo ao nuget

---
+++ CollectionViewExemplo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewExemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new MainViewModel();
+        }
+
+        void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            cvGames.ScrollTo((int)Math.Floor(e.NewValue));
+        }
+    }
+}
---
+++ CollectionViewExemplo/ViewModels/MainViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CollectionViewExemplo
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        #region Property
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #endregion
+
+        public ObservableCollection<Game> Games { get; }
+
+        private int _rating;
+        public int Rating
+        {
+            get { return _rating; }
+            set => SetProperty(ref _rating, value);
+        }
+
+        public MainViewModel()
+        {
+            Games = new ObservableCollection<Game>();
+
+            Games.Add(new Game
+            {
+                Nome = "God of War",
+                TituloBR = "O bom da guerra"
+            });
+
+            Games.Add(new Game
+            {
+                Nome = "The last of us",
+                TituloBR = "Nois que sobramus"
+            });
+
+            Games.Add(new Game
+            {
+                Nome = "Mario Kart 8",
+                TituloBR = "Correndo com meus Amig..Inimigos"
+            });
+
+            Games.Add(new Game
+            {
+                Nome = "Super Mario Odyssey",
+                TituloBR = "As viagens do Bigode"
+            });
+
+            Games.Add(new Game
+            {
+                Nome = "Halo",
+                TituloBR = "E eu passei metiolate"
+            });
+
+        }
+
+    }
+}
